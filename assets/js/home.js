0a2e7adc13e9f1611e728fda71d443c3e0bdf180
Pablo BADIA <pab.badia@gmail.com>
2020-08-17 18:02:05 +0200

    Install and setup fontawesome

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,14 +3,17 @@ import '../css/home.scss';
 // Core variables and mixins
 import "../css/_variables.scss";
 import "../css/_mixins.scss";
-// Global CSS
-import "../css/_global.scss";
+
+// Main scss
+import '../css/app.scss';
+
 // Components
+import '../css/_medals.scss';
+import '../css/_buttons.scss';
+import '../css/_navbar.scss';
 import "../css/_masthead.scss";
 import "../css/_showcase.scss";
 import "../css/_icons.scss";
 import "../css/_testimonials.scss";
 import "../css/_call-to-action.scss";
 import "../css/_footer.scss";
-import "../css/_variables.scss";
-
